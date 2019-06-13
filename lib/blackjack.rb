90388f2572e1e39d23d1d75b75b0413e303f3e73
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-13 15:59:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -4,7 +4,6 @@ end
 
 def deal_card
   rand(1..11)
-  
 end
 
 def display_card_total(card_total)
@@ -13,11 +12,10 @@ end
 
 def prompt_user
   puts "Type 'h' to hit or 's' to stay"
-    
 end
 
 def get_user_input
-  gets.chomp
+ return gets.chomp
 end
 
 def end_game(card_total)
